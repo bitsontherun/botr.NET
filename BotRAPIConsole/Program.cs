2920c368e1dacb3a810b1fdb949b5fe038c6c6a9
Wes Cilldhaire <wes@sol1.com.au>
2011-09-21 17:00:25 +1000

    Renamed license file for clarity.
    Appended license information to source, project, and solution files.

--- BotRAPIConsole/Program.cs
+++ BotRAPIConsole/Program.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+ * This file is part of the Bits on the Run .Net API Library.
+ * 
+ * The Bits on the Run .Net API Library is free software: you
+ * can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * The Bits on the Run .Net API Library is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with the Bits on the Run .Net API
+ * Library.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
